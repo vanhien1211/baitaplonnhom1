c838d12f7c2de04facbbcf28464491d6c5bb150b
Hien <137436310+vanhien1211@users.noreply.github.com>
2023-12-28 21:00:58 +0700

    Update main.py

--- BTL_MNM/main.py
+++ BTL_MNM/main.py
@@ -1,27 +1,30 @@
 import tkinter as tk
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 def open_basic_calculator():
-    # Chạy mã code giao diện máy tính cơ bản từ một tệp riêng
     subprocess.call(["python", "basic_calculator.py"])
 
 def open_analysis_app():
-    # Chạy mã code giao diện ứng dụng hỗ trợ giải tích từ một tệp riêng
     subprocess.call(["python", "analysis_app.py"])
 
-# Tạo cửa sổ chính
+def open_app_with_executor(executor, app_function):
+    executor.submit(app_function)
+
 window = tk.Tk()
-window.config(bg='lightblue')
-# Thiết lập giao diện chính
-label = tk.Label(window, text="Chọn ứng dụng:",bg='lightgreen')
+window.resizable(width=False, height=False)
+
+label = tk.Label(window, text="Chọn ứng dụng:")
 label.pack()
 
-# Tạo các nút lựa chọn
-button_basic_calculator = tk.Button(window, text="Máy tính cơ bản", command=open_basic_calculator,bg='yellow')
+executor = ThreadPoolExecutor()
+
+button_basic_calculator = tk.Button(window, text="Máy tính cơ bản", command=lambda: open_app_with_executor(executor, open_basic_calculator))
 button_basic_calculator.pack()
 
-button_analysis_app = tk.Button(window, text="Ứng dụng hỗ trợ giải tích", command=open_analysis_app,bg='yellow')
+button_analysis_app = tk.Button(window, text="Ứng dụng hỗ trợ giải tích", command=lambda: open_app_with_executor(executor, open_analysis_app))
 button_analysis_app.pack()
 
-# Chạy giao diện
 window.mainloop()
+
+executor.shutdown()
